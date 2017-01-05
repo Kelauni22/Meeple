4f707fb6f590b51e3c57de15ce41e64697bb6981
Kelauni22 <Kelauni.Cook@gmail.com>
2017-01-05 17:08:26 -0500

    Initial commit of all files for Meeple incl 'demo' virtualenv

---
+++ demo/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\kelaun~1\desktop\meeple\demo\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ ex1.py
@@ -0,0 +1,9 @@
+print "How old are you? (years)",
+age = input()
+print "How tall are you? (inches)",
+height = input()
+print "How much do you weigh? (pounds)",
+weight = input()
+
+
+print "So, you're %s years old, %s inches tall, and %s pounds" % (age, height, weight)
