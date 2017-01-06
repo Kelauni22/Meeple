9f8d4035388a674a0c96d441ddd39bf96f9ed810
Kelauni22 <Kelauni.Cook@gmail.com>
2017-01-06 14:56:39 -0500

    Boardgames: Added a view function, a url home request to urls.py, and a template called 'home.html' to render 'Hello, Kelauni!'

--- demo/boardgames/main/views.py
+++ demo/boardgames/main/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request): #This is a view in Django
+    return render(request, "main/home.html")
